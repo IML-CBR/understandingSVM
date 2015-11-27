d1a48b4783fccf3127aa06ecad37d10cca9cf1d5
Julia Camps <julcamps@gmail.com>
2015-11-27 14:12:09 +0100

    block1

--- Code/train_linearSVMhard.m
+++ Code/train_linearSVMhard.m
@@ -1,13 +1,21 @@
-function [ model, other_values ] = train_linearSVMhard( labels, data, params )
+function [ w , d ] = train_linearSVMhard( labels, data )
 % TODO
     m = size(data,1);
-    n = size(data,2);
-    A = [data,ones(m,1)];
-    cvx_begin
-        variable x(n+1)
-        minimize( norm( A*x, 2 )/2 )
+    n = size(data,2)+1;
+    A = [ones(m,1),data];
+%     d = zeros(1,20);
+    cvx_begin 
+        variable w(n)
+        minimize( norm( w, 2 )/2 )
+        d = A*w
+        for i=1:m
+            d(i) = (d(i)*labels(i)).^0.5
+        end
         subject to
-            x'*A*labels >= 1
+            d >= 1
     cvx_end
+    for i=1:m
+        d(i) = roundx(d(i),5,'round');
+    end
 end
 
