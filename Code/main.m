918aa3ccd4f7b8a22ca4e2939aac0fde4fbdeec2
xserra93 <x.serra93@gmail.com>
2015-12-08 14:42:59 +0100

    Question blocks 5 and 6 finished

--- Code/main.m
+++ Code/main.m
@@ -2,10 +2,13 @@
 clear all;
 close all;
 clc;
+
 %% Move to working directory
 tmp = matlab.desktop.editor.getActive;
 cd(fileparts(tmp.Filename));
-%% QUESTION 1 - Juli
+
+
+%% QUESTION 1
 % 1)
 Dataset = load('../Data/example_dataset_1');
 labels = Dataset.labels;
@@ -17,7 +20,8 @@ model
 name = 'linear SVM hard';
 plotSVM( data, labels, model, name );
 
-%% QUESTION 2 - Juli
+
+%% QUESTION 2
 % 1)
 Dataset = load('../Data/example_dataset_1');
 labels = Dataset.labels;
@@ -63,8 +67,7 @@ suport_indexes = find(arrayfun(@(x) roundx(x,5,'round'),(distances))==1)
 suports = data(suport_indexes,:)
 
 
-
-%% QUESTION 3 - Juli
+%% QUESTION 3
 % 1)
 Dataset = load('../Data/example_dataset_2');
 labels = Dataset.labels;
@@ -95,7 +98,8 @@ distances = ([data,ones(size(data,1),1)]*model).*labels
 suport_indexes = find(arrayfun(@(x) roundx(x,5,'round'),(distances))==1)
 suports = data(suport_indexes,:)
 
-%% QUESTION 4 - Juli
+
+%% QUESTION 4
 Dataset = load('../Data/example_dataset_2');
 labels = Dataset.labels;
 data = Dataset.data';
@@ -127,35 +131,76 @@ v(find(v > 0))
 % SVs:
 data(find(v > 0),:)
 
-%% QUESTION 5 - Xavi
+
+%% QUESTION 5
 dataset_5 = load('../Data/example_dataset_3');
 data_5 = dataset_5.data';
 labels_5 = dataset_5.labels;
 
 % Num examples each class
-label_pos = sum(labels_5==1)
-label_neg = sum(labels_5==-1)
+num_pos = sum(labels_5==1)
+num_neg = sum(labels_5==-1)
 
 decimals = 5;
-lambdas = [0.1 0 1 10 100];
+lambdas = [1 10 100 1000 10000];
+errors_t = zeros(1,length(lambdas));
 best_error = Inf;
 for i = 1:length(lambdas)
     lambda = lambdas(i);
     model_5 = train_linearSVMsoft( labels_5, data_5, lambda );
     distances = ([data_5,ones(size(data_5,1),1)]*model_5).*labels_5;
-    errors = data_5(find(arrayfun(@(x) roundx(x,decimals,'round'),(distances))<1),:);
-    if size(errors,1) < best_error
-        best_error = size(errors,1);
+    
+    error_svm = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<1);
+    error_mis = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<0);
+    errors_svm(i) = size(error_svm,1);
+    errors_mis(i) = size(error_mis,1);
+    if size(errors_svm,1) < best_error
+        best_error = size(errors_svm,1);
         best_model = model_5;
         best_lambda = lambda;
     end
 end
 
-distances = ([data_5,ones(size(data_5,1),1)]*best_model).*labels_5;
-error_labels = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<1);
-plotSVM( data_5, labels_5, best_model, name );
-    
-%% QUESTION 6 - Xavi
 
-model_5 = train_linearSVMweighted( labels_5, data_5, lambda );
-plotSVM( data_5, labels_5, model_5, name );
+distances = ([data_5,ones(size(data_5,1),1)]*best_model).*labels_5;
+error_svm_labels = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<1);
+error_labels = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<0);
+plotSVM( data_5, labels_5, best_model, 'Soft SVM with unbalanced data' );
+
+
+%% QUESTION 6
+dataset_5 = load('../Data/example_dataset_3');
+data_5 = dataset_5.data';
+labels_5 = dataset_5.labels;
+
+% Num examples each class
+num_pos = sum(labels_5==1)
+num_neg = sum(labels_5==-1)
+
+w1 = 1-num_pos/(num_pos+num_neg)
+w2 = 1-w1
+
+decimals = 5;
+lambdas = [1 10 100 1000 10000];
+best_error = Inf;
+errors_svm = zeros(1,length(lambdas));
+errors_mis = zeros(1,length(lambdas));
+for i = 1:length(lambdas)
+    lambda = lambdas(i);
+    model_5_w = train_linearSVMweighted( labels_5, data_5, lambda, w1,w2 );
+    distances = ([data_5,ones(size(data_5,1),1)]*model_5_w).*labels_5;
+    error_svm = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<1);
+    error_mis = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<0);
+    errors_svm(i) = size(error_svm,1);
+    errors_mis(i) = size(error_mis,1);
+    if size(error_svm_labels,1) < best_error
+        best_error = size(error_svm_labels,1);
+        best_model = model_5_w;
+        best_lambda = lambda;
+    end
+end
+distances = ([data_5,ones(size(data_5,1),1)]*best_model).*labels_5;
+error_svm_labels = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<1);
+error_labels = labels_5(arrayfun(@(x) roundx(x,decimals,'round'),distances)<0);
+
+plotSVM( data_5, labels_5, best_model, 'Weighted SVM with unbalanced data' );
--- Code/train_linearSVMweighted.m
+++ Code/train_linearSVMweighted.m
@@ -1,17 +1,19 @@
-function model = train_linearSVMweighted( labels, data, lambda )
+function model = train_linearSVMweighted( labels, data, lambda, w1, w2 )
     % This function trains a weighted SVM
     
     n = size(data,2);
     r = data(labels==-1,:);
     s = data(labels==1,:);
     m = size(s,1);
+    k = size(r,1);
     cvx_begin
-        variables model(n) b(1) u(m)
-        minimize norm(model,2)+lambda*(ones(1,m)*u)
+        variables model(n) b(1) u(m) v(k)
+        minimize norm(model,2)+lambda*(ones(1,m)*u*w1+ones(1,k)*v*w2)
         subject to
-            (r*model + b) <= -1
+            (r*model + b) <= -1+v
             (s*model + b) >= 1-u
             u >= 0
+            v >= 0
     cvx_end
     model(end+1) = b;
 end
